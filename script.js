ce1863659df925b779804974e8a58066ea8363be
Patryk Czyzkowski <patryk.czyzkowskir1@gmail.com>
2023-10-04 20:56:24 +0200

    cala totolist

--- script.js
+++ script.js
@@ -14,3 +14,14 @@ function addTask() {
   }
   input.value = "";
 }
+listcontainer.addEventListener(
+  "click",
+  function (e) {
+    if (e.target.tagName === "LI") {
+      e.target.classList.toggle("checked");
+    } else if (e.target.tagName === "SPAN") {
+      e.target.parentElement.remove();
+    }
+  },
+  false
+);
