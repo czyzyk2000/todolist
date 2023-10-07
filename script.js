6faee1b1c8592526715f70636ea9bc994336ba6b
Patryk Czyzkowski <patryk.czyzkowskir1@gmail.com>
2023-10-07 13:44:28 +0200

    dodanie funkcjonalnosci przyciskow oraz filtrowanie

--- script.js
+++ script.js
@@ -1,8 +1,68 @@
-const input = document.getElementsByClassName("addInput")[0];
-const listContainer = document.getElementsByClassName("listContainer")[0];
+const input = document.querySelector(".addInput");
+const listContainer = document.querySelector(".listContainer");
+const completedListContainer = document.querySelector(
+  ".completedListContainer"
+);
+const allButton = document.querySelector(".allButton");
+const clearCompletedButton = document.querySelector(".clearCompleted");
+allButton.addEventListener("click", function () {
+  filterTasks("all");
+});
+const tasks = [];
 
-function createListItem(taskContent) {
+function renderTasks(filteredTasks) {
+  listContainer.innerHTML = "";
+
+  filteredTasks.forEach((task) => {
+    const listItem = createListItem(task.title, task.status);
+    listContainer.appendChild(listItem);
+  });
+}
+
+clearCompletedButton.addEventListener("click", function () {
+  clearAllTasks();
+});
+
+function clearAllTasks() {
+  tasks.length = 0;
+  renderTasks([]);
+}
+
+function filterTasks(status) {
+  const filteredTasks = tasks.filter((task) => {
+    if (status === "all") {
+      return task;
+    } else if (status === task.status) {
+      return task;
+    }
+  });
+
+  renderTasks(filteredTasks);
+}
+
+function createListItem(taskContent, status) {
   const listItem = document.createElement("li");
+
+  listItem.textContent = taskContent;
+
+  if (status === "completed") {
+    listItem.classList.add("completed");
+  }
+
+  return listItem;
+}
+
+function clearCompletedTasks() {
+  const activeTasks = tasks.filter((task) => task.status === "active");
+  tasks.length = 0; // Wyczyszczenie tablicy
+  tasks.push(...activeTasks); // Dodanie z powrotem aktywnych zadań
+  renderTasks(tasks);
+}
+
+function createListItem(taskContent, id) {
+  const listItem = document.createElement("li");
+  listItem.setAttribute("id", id);
+
   listItem.textContent = taskContent;
 
   const deleteButton = document.createElement("span");
@@ -11,9 +71,9 @@ function createListItem(taskContent) {
 
   return listItem;
 }
-
+//////////////////////////////
 if (listContainer) {
-  listContainer.addEventListener("click", handleListClick, false);
+  listContainer.addEventListener("click", handleListClick);
 }
 
 function handleListClick(event) {
@@ -21,19 +81,37 @@ function handleListClick(event) {
 
   if (clickedElement.tagName === "LI") {
     clickedElement.classList.toggle("checked");
+    tasks.map((task) => {
+      if (task.id === +clickedElement.id) {
+        task.status = task.status === "active" ? "completed" : "active";
+      }
+      return task;
+    });
   } else if (clickedElement.tagName === "SPAN") {
     clickedElement.parentElement.remove();
   }
 }
 
+input.addEventListener("keypress", function (event) {
+  if (event.key === "Enter") {
+    event.preventDefault();
+    addTask();
+  }
+});
+
 function addTask() {
   const taskInputValue = input.value.trim();
-
   if (taskInputValue === "") {
-    const EMPTY_INPUT_MESSAGE = "Musisz coś wpisać";
-    alert(EMPTY_INPUT_MESSAGE);
+    alert("Wprowadzono pusty tekst");
   } else {
-    const listItem = createListItem(taskInputValue);
+    const id = new Date().valueOf();
+    const listItem = createListItem(taskInputValue, id);
+
+    tasks.push({
+      id: id,
+      title: taskInputValue,
+      status: "active",
+    });
     if (listContainer) {
       listContainer.appendChild(listItem);
     }
